16ddc5042ec25c846ab8cae946819e7e9c37db3f
Alaa-Alotaibi <57038620+Alaa-Alotaibi@users.noreply.github.com>
2026-06-02 13:47:44 +0300

    Implement average height calculation
    Calculate total height and average height of students.

---
+++ Day_05_Python_Loops/ex_009_average_height.py
@@ -0,0 +1,20 @@
+student_heights = input("Input a list of student heights: ").split()
+
+for n in range(len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+total_height = 0
+
+for height in student_heights:
+    total_height += height
+
+number_of_students = 0
+
+for student in student_heights:
+    number_of_students += 1
+
+average_height = round(total_height / number_of_students)
+
+print(f"total height = {total_height}")
+print(f"number of students = {number_of_students}")
+print(f"average height = {average_height}")
